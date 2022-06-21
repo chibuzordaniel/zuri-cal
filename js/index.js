32a74ab69911700d45954c484d3b408c494e71c8
chibuzordaniel <starchrischibuzor@gmail.com>
2022-06-22 00:40:21 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+  // read numbers in the program
+  const num1 = parseFloat(prompt('Enter number1'));
+  const num2 = parseFloat(prompt('Enter number2'));
+  // read operator
+  const operator = prompt('Enter operator(+, -, /, *)');
+  let result = 0;
+  if(isNaN(num1) || isNaN(num2)){
+      alert('Wrong Input Refresh the page again and privide data! ');
+  }else{
+
+      if(operator == '+'){
+          result = num1 + num2;
+      }else if(operator == '-'){
+          result = num1 - num2;
+      }else if(operator == '*'){
+          result = num1 * num2;
+      }else if(operator == '/'){
+          result = num1 / num2;
+      }
+
+      prompt(num1 + operator + num2 + ' = ' + result); 
+  }
